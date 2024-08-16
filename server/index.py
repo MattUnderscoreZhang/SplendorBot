d7dc219eaa7797b670ff736d0d354cb2f17ebfe0
Matt Zhang <set.stun@gmail.com>
2024-08-15 19:19:40 -0500

    Added noble card

--- server/index.py
+++ server/index.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from splendor_bot.game import new_game
-from splendor_bot.types import Card
+from splendor_bot.types import Card, Noble
 
 
 app = FastAPI()
@@ -11,8 +11,6 @@ templates = Jinja2Templates(directory="templates")
 
 
 def card_html(request: Request, card: Card) -> HTMLResponse:
-    game_state = new_game(n_players=2)
-    card = game_state.revealed_cards_by_level[0][0]
     return templates.TemplateResponse(
         request=request,
         name="card.html",
@@ -22,8 +20,20 @@ def card_html(request: Request, card: Card) -> HTMLResponse:
     )
 
 
+def noble_html(request: Request, noble: Noble) -> HTMLResponse:
+    return templates.TemplateResponse(
+        request=request,
+        name="noble.html",
+        context={
+            "noble": noble,
+        }
+    )
+
+
 @app.get("/", response_class=HTMLResponse)
 def game(request: Request):
     game_state = new_game(n_players=2)
-    card = game_state.revealed_cards_by_level[0][0]
-    return card_html(request, card)
+    # card = game_state.revealed_cards_by_level[0][0]
+    # return card_html(request, card)
+    noble = game_state.nobles[0]
+    return noble_html(request, noble)
